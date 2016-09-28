c9cef5ab7f59189a305d1dec94a50b03b43841fe
Mary Kuzmenko <mary.kuzmenko@gmail.com>
2016-09-28 21:01:13 +0300

    Update list-elements.py

--- list-elements.py
+++ list-elements.py
@@ -1,10 +1,9 @@
-x = ["a", "b", "c", "d"]
-x[1] = "r"
-x[2:] = ["s", "t"]
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0, 
+         "bedroom", 10.75, "bathroom", 10.50]
 
-print x
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
 
-x = ["a", "b", "c", "d"]
-y = x + ["e", "f"]
-
-print y
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
