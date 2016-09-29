0760373f77a21c80f6a215a20e5f0395c479e612
seleznova <mary.kuzmenko@gmail.com>
2016-09-29 15:04:27 +0300

    Update for list-elements.py

--- list-elements.py
+++ list-elements.py
@@ -7,3 +7,12 @@ areas_1 = areas + ["poolhouse", 24.5]
 
 # Add garage data to areas_1, new list is areas_2
 areas_2 = areas_1 + ["garage", 15.45]
+
+areas = ["hallway", 11.25, "kitchen", 18.0,
+         "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+del(areas[10]); del(areas[11])
+
+print (areas)
