66c42d4c97561aae90e3008dc98a5b13a9162163
seleznova <mary.kuzmenko@gmail.com>
2016-09-29 18:48:37 +0300

    Add file functions.py

--- list-elements.py
+++ list-elements.py
@@ -13,6 +13,8 @@ areas = ["hallway", 11.25, "kitchen", 18.0,
          "bathroom", 10.50, "poolhouse", 24.5,
          "garage", 15.45]
 
-del(areas[10]); del(areas[11])
+del(areas[-4:-2])
+
+areas_new = list(areas)
 
 print (areas)
