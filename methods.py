dbd07dce2460661fe10f49db57ef87086d114204
seleznova <mary.kuzmenko@gmail.com>
2016-09-29 19:44:25 +0300

    Add file methods.py

---
+++ methods.py
@@ -0,0 +1,20 @@
+phrase = "i love you, my life"
+print (type(phrase))
+
+number = 1984.11
+
+array = ["liz", 1.73, "emma", 1.68,
+        "mom", 1.71, "dad", 1.89]
+
+print (array)
+
+print (array.index("emma"))
+print (array.count(1.73))
+
+print (phrase.capitalize())
+
+print (phrase.replace("life", "honey"))
+print (array)
+array.append("me")
+array.append("1.61")
+print (array)
