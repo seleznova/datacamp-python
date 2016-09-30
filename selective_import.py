c8ac72106d2d847dd01a9a59d02e882c47d649fd
seleznova <mary.kuzmenko@gmail.com>
2016-09-30 19:45:07 +0300

    New examples of Python use

---
+++ selective_import.py
@@ -0,0 +1,16 @@
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon if 12 degrees. Store in dist.
+dist = r * radians(12)
+
+# Print out dist
+print (dist)
+
+import scipy.linalg import my_inv as my_inv
+x = my_inv([[1,2], [3,4]])
+
+print (x)
