c5ee52f56d21c62e2420a4e9bf70dfc29f05300f
seleznova <mary.kuzmenko@gmail.com>
2016-10-03 18:46:56 +0300

    Update in numpy_assignment.py

---
+++ numpy_assignments.py
@@ -0,0 +1,30 @@
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a Numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+# Exercice 2
+
+# height is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a Numpy array from height: np_height
+np_height = np.array(height)
+
+# Print out np_height
+print(np_height)
+
+# Convert np_height to m: np_height_m
+np_height_m = np_height * 0.0254
+
+# Print np_height_m
+print(np_height_m)
