b87494ceb726399467865775065d0d8e201eb20d
seleznova <mary.kuzmenko@gmail.com>
2016-10-03 22:15:24 +0300

    Update 2d-numpy-arrays.py

--- 2d-numpy-arrays.py
+++ 2d-numpy-arrays.py
@@ -6,3 +6,10 @@ np_weight = np.array([65.4, 58.2, 70.3])
 
 print (type(np_height))
 print (type(np_weight))
+
+np_2d = np.array([[1.73, 1.68, 1.82],
+                  [65.4, 58.2, 70.3]])
+
+print (np_2d)
+print (np_2d.shape)
+
