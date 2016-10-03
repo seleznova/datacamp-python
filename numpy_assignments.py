9a8386aa28eccd6b0b6f9acddc2aa98c0e7b4f5b
seleznova <mary.kuzmenko@gmail.com>
2016-10-03 19:09:13 +0300

    Update in numpy_assignment.py

--- numpy_assignments.py
+++ numpy_assignments.py
@@ -70,3 +70,20 @@ print(bmi[light])
 # Quiz
 
 # Correct answer: np.array([4, 3, 0]) + np.array([0, 2, 2])
+
+# Next exercise
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight = np.array(weight)
+np_height = np.array(height)
+
+# Print out the weight at index 50
+print(np_weight[50])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height[100:111])
