620c73f8110ad15d562c9bc426195a39643f959e
seleznova <mary.kuzmenko@gmail.com>
2016-10-04 22:32:51 +0300

    Updates in 2d-numpy-exercises.py

--- 2d-numpy-arrays-assignment.py
+++ 2d-numpy-arrays-assignment.py
@@ -18,3 +18,15 @@ print(type(np_baseball))
 # Print out the shape of np_baseball
 print(np_baseball.shape)
 
+# Exercise 2
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D Numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
