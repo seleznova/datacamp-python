005cfaa3e59727aaca8f6d8286f22f89a7ced772
seleznova <mary.kuzmenko@gmail.com>
2016-10-04 22:43:29 +0300

    Updates in 2d-numpy.py and 2d-numpy-exercises.py

--- 2d-numpy-arrays-assignment.py
+++ 2d-numpy-arrays-assignment.py
@@ -49,3 +49,22 @@ np_weight = np_baseball[:, 1]
 
 # Print out height of 124th player
 print(np_baseball[123, 0])
+
+# Exercise 4
+# baseball is available as a regular list of lists
+# update is available as 2D Numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and update
+print(np_baseball + update)
+
+# Create Numpy array: conversion
+conversion = [0.0254, 0.453592, 1]
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
--- 2d-numpy-arrays.py
+++ 2d-numpy-arrays.py
@@ -24,3 +24,9 @@ print (np_2d[:, 1:3])
 print (np_2d[1, :])
 
 
+np_mat = np.array([[1, 2],
+                   [3, 4],
+                   [5, 6]])
+np_mat * 2
+print(np_mat + np.array([10, 10]))
+print(np_mat + np_mat)
