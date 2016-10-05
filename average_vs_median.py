7e077589f425e205182f0ddfa3c490c54c967fd0
seleznova <mary.kuzmenko@gmail.com>
2016-10-05 18:25:04 +0300

    Add new file average_vs_median.py

---
+++ average_vs_median.py
@@ -0,0 +1,38 @@
+# Exercise 1
+
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Create np_height from np_baseball
+np_height = np.array(np_baseball[:,0])
+
+# Print out the mean of np_height
+print(np.mean(np_height))
+
+# Print out the median of np_height
+print(np.median(np_height))
+
+# Exercise 2
+
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])
+print("Correlation: " + str(corr))
